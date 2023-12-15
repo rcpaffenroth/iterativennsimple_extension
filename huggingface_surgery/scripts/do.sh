0901d6936cb8086f49d8c9dfdf770a6c92291d71
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-15 08:55:17 -0500

    Small changes

--- huggingface_surgery/scripts/do.sh
+++ huggingface_surgery/scripts/do.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-COMMAND="train_and_analyze.py --model-name bert-base-cased --cuda"
+# COMMAND="train_and_analyze.py --model-name bert-base-cased --cuda"
+# rcp n "$COMMAND started"
+# python $COMMAND
+# rcp n "$COMMAND ended"
+# 
+# COMMAND="train_and_analyze.py --model-name bert-base-cased"
+# rcp n "$COMMAND started"
+# python $COMMAND
+# rcp n "$COMMAND ended"
+
+COMMAND="train_and_analyze.py --model-name mistralai/Mistral-7B-v0.1 --cuda" 
 rcp n "$COMMAND started"
 python $COMMAND 
 rcp n "$COMMAND ended"
 
-COMMAND="train_and_analyze.py --model-name bert-base-cased"
-rcp n "$COMMAND started"
-python $COMMAND 
-rcp n "$COMMAND ended"
-
-COMMAND="train_and_analyze.py --model-name bert-base-uncased --cuda" 
-rcp n "$COMMAND started"
-python $COMMAND 
-rcp n "$COMMAND ended"
-
-COMMAND="train_and_analyze.py --model-name bert-base-uncased"
+COMMAND="train_and_analyze.py --model-name mistralai/Mistral-7B-v0.1"
 rcp n "$COMMAND started"
 python $COMMAND 
 rcp n "$COMMAND ended"
