34c998ae3341fd36a4754cff7d1a60b2bcadcb97
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-15 07:22:44 -0500

    Added messaging

--- huggingface_surgery/scripts/do.sh
+++ huggingface_surgery/scripts/do.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
-echo "train_and_analyze.py --model-name bert-base-cased --cuda"
-python train_and_analyze.py --model-name bert-base-cased --cuda 
-echo "train_and_analyze.py --model-name bert-base-cased"
-python train_and_analyze.py --model-name bert-base-cased
-echo "train_and_analyze.py --model-name bert-base-uncased --cuda" 
-python train_and_analyze.py --model-name mistralai/Mistral-7B-v0.1 --cuda 
-echo "train_and_analyze.py --model-name bert-base-uncased"
-python train_and_analyze.py --model-name mistralai/Mistral-7B-v0.1 
+COMMAND="train_and_analyze.py --model-name bert-base-cased --cuda"
+rcp n "$COMMAND started"
+python $COMMAND 
+rcp n "$COMMAND ended"
+
+COMMAND="train_and_analyze.py --model-name bert-base-cased"
+rcp n "$COMMAND started"
+python $COMMAND 
+rcp n "$COMMAND ended"
+
+COMMAND="train_and_analyze.py --model-name bert-base-uncased --cuda" 
+rcp n "$COMMAND started"
+python $COMMAND 
+rcp n "$COMMAND ended"
+
+COMMAND="train_and_analyze.py --model-name bert-base-uncased"
+rcp n "$COMMAND started"
+python $COMMAND 
+rcp n "$COMMAND ended"
