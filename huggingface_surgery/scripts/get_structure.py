5cbbda71b9400a3a8849ebb952f4ce4c57af5561
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-22 17:12:10 -0500

    Small changes

--- huggingface_surgery/scripts/get_structure.py
+++ huggingface_surgery/scripts/get_structure.py
@@ -1,5 +1,6 @@
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoModelForCausalLM
+from transformers import AutoConfig
 #model = AutoModelForSequenceClassification.from_pretrained("t5-base")
 #model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased")
 #model = AutoModelForSequenceClassification.from_pretrained("mistralai/Mistral-7B-v0.1")
@@ -8,6 +9,15 @@ from transformers import AutoModelForCausalLM
 
 # print(model)
 
+# model = AutoConfig.from_pretrained("tiiuae/falcon-40b")
+# print(model)
+
 model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b")
 print(model)
+
+# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b")
+# print(model)
+
+# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b")
+# print(model)
 
