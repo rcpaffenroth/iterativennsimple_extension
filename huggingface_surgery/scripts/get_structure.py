f896eb77043aa01f83d8cbf0165fc4be109c2383
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-25 18:42:19 +0000

    Small changes

--- huggingface_surgery/scripts/get_structure.py
+++ huggingface_surgery/scripts/get_structure.py
@@ -1,23 +1,11 @@
-from transformers import AutoModelForSequenceClassification
 from transformers import AutoModelForCausalLM
-from transformers import AutoConfig
-#model = AutoModelForSequenceClassification.from_pretrained("t5-base")
-#model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased")
-#model = AutoModelForSequenceClassification.from_pretrained("mistralai/Mistral-7B-v0.1")
-#model = AutoModelForSequenceClassification.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
-# model = AutoModelForSequenceClassification.from_pretrained("tiiuae/falcon-rw-1b")
 
-# print(model)
-
-# model = AutoConfig.from_pretrained("tiiuae/falcon-40b")
-# print(model)
-
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b")
+model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b", cache_dir="/home/rcpaffenroth/projects/quick_project/huggingface_surgery/cache")
 print(model)
 
-# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b")
-# print(model)
+model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b", cache_dir="/home/rcpaffenroth/projects/quick_project/huggingface_surgery/cache")
+print(model)
 
-# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b")
+# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b", cache_dir="/home/rcpaffenroth/projects/quick_project/huggingface_surgery/cache")
 # print(model)
 
