3d56cb8e894fcc357500fccffac9cc324b58602f
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-27 09:20:50 -0500

    Small changes and default config for russ

--- huggingface_surgery/scripts/get_structure.py
+++ huggingface_surgery/scripts/get_structure.py
@@ -5,9 +5,9 @@ cache_dir = "/home/rcpaffenroth/projects/quick_project/huggingface_surgery/scrip
 model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b", cache_dir=cache_dir)
 print(model)
 
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b", cache_dir=cache_dir)
-print(model)
+# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b", cache_dir=cache_dir)
+# print(model)
 
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b", cache_dir=cache_dir)
-print(model)
+# model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-40b", cache_dir=cache_dir)
+# print(model)
 
