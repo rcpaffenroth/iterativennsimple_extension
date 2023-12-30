ff34ccacb779814b3af72c3a5ff46c26ad2edf74
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-30 16:17:30 +0000

    Fixed up directories for melnibone

--- huggingface_surgery/scripts/download_datasets_and_models.py
+++ huggingface_surgery/scripts/download_datasets_and_models.py
@@ -2,31 +2,27 @@
 # %%
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM
+from pathlib import Path
 
-data_download_list=['tiiuae/falcon-refinedweb', 'ptb_text_only']
-# data_download_list=['ptb_text_only']
+base_dir = Path('/mnt/research/rpaffenroth/data')
+cache_dir = base_dir / 'huggingface_cache'
+save_dir = base_dir / 'huggingface_downloads'
+
+# data_download_list=['tiiuae/falcon-refinedweb', 'ptb_text_only']
+data_download_list=['ptb_text_only']
 
 # %%
 for dataset_name in data_download_list:
-    dataset = load_dataset(dataset_name, cache_dir='/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_cache')
-    dataset.save_to_disk('/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_downloads/'+dataset_name)
-
-# # %%
-# for dataset_name in data_download_list:
-#     dataset = load_dataset('/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_downloads/'+dataset_name)
+    dataset = load_dataset(dataset_name, 
+                            cache_dir=cache_dir
+    dataset.save_to_disk(save_dir / dataset_name)
 
 # %%
-model_download_list=['tiiuae/falcon-rw-1b', 'tiiuae/falcon-7b', 'tiiuae/falcon-40b']
-# model_download_list=['tiiuae/falcon-rw-1b']
+# model_download_list=['tiiuae/falcon-rw-1b', 'tiiuae/falcon-7b', 'tiiuae/falcon-40b']
+model_download_list=['tiiuae/falcon-rw-1b']
 
 # %%
 for model_name in model_download_list:
-    model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir='/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_cache')
-    model.save_pretrained('/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_downloads/'+model_name)
-
-# # %%
-# for model_name in model_download_list:
-#     model = AutoModelForCausalLM.from_pretrained('/home/rcpaffenroth/projects/quick_project/huggingface_surgery/huggingface_downloads/'+model_name)
-
-
-# %%
+    model = AutoModelForCausalLM.from_pretrained(model_name, 
+                            cache_dir=cache_dir)
+    model.save_pretrained(save_dir / model_name)
