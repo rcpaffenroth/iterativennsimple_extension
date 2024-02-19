0074eba5a70157ac10007f3737e77501ef8e73db
Randy Paffenroth <randy.paffenroth@gmail.com>
2024-02-19 18:32:34 -0500

    More cleanups

---
+++ iterativennsimple_extension/mycode.py
@@ -0,0 +1,8 @@
+# Some code that you want to test
+from iterativennsimple.MaskedLinear import MaskedLinear
+
+def myfunc():
+    # Create a MaskedLinear layer
+    layer = MaskedLinear(10, 20)
+    # Do something with the layer
+    return layer
